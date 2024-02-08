c9f366637abbce3cae02f7563051367eda5fd0d9
Kim Hwanhoon <kimhwanhoon@outlook.com>
2024-02-08 08:46:49 +0100

    otp error handling clientside alert has been set

--- src/components/auth/OTP.tsx
+++ src/components/auth/OTP.tsx
@@ -1,10 +1,23 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
-import { Button, Input, Progress } from "@nextui-org/react";
+import {
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Progress,
+  useDisclosure,
+} from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import OTPInput from "react-otp-input";
+import { Alert } from "../alerts/alert";
+import useToggleModal from "@/utils/store/alertContents";
+import useAlertContents from "@/utils/store/alertContents";
 
 interface Props {
   email: string;
@@ -14,6 +27,13 @@ export const OTP = ({ email }: Props) => {
   const [otp, setOtp] = useState("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [progress, setProgress] = useState<number>(0);
+
+  // alert
+  // const { toggleModal, setToggleModal } = useToggleModal();
+  // setToggleModal(true);
+  const { onOpen, isOpen, onClose, onOpenChange } = useDisclosure();
+  const { setAlertProps } = useAlertContents();
+  //
   const router = useRouter();
 
   const handlePaste: React.ClipboardEventHandler = (event) => {
@@ -39,9 +59,18 @@ export const OTP = ({ email }: Props) => {
 
     if (message === "Verify requires either a token or a token hash") {
       console.log("error");
+
+      setAlertProps(
+        "error",
+        "Code Error",
+        "Please check your code and try again."
+      );
+      onOpen();
     }
     if (message === "Token has expired or is invalid") {
       console.log("error");
+      setAlertProps("error", "Token Error", "Token has expired or is invalid.");
+      onOpen();
     }
     if (!error) {
       router.replace("/welcome");
@@ -55,21 +84,30 @@ export const OTP = ({ email }: Props) => {
   }, [otp]);
 
   return (
-    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-12">
-      <div className="relative bg-white px-6 pt-10 pb-9 shadow-xl mx-auto w-full max-w-lg rounded-2xl">
-        <div className="mx-auto flex w-full max-w-md flex-col space-y-16">
-          <div className="flex flex-col items-center justify-center text-center space-y-2">
-            <div className="font-semibold text-3xl">
-              <p>Email Verification</p>
+    <>
+      {/*  */}
+      <Alert
+        key={"alert"}
+        title=""
+        body=""
+        useDisclosure={{ onOpen, isOpen, onClose, onOpenChange }}
+      />
+      {/*  */}
+      <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-12">
+        <div className="relative bg-white px-6 pt-10 pb-9 shadow-xl mx-auto w-full max-w-lg rounded-2xl">
+          <div className="mx-auto flex w-full max-w-md flex-col space-y-16">
+            <div className="flex flex-col items-center justify-center text-center space-y-2">
+              <div className="font-semibold text-3xl">
+                <p>Email Verification</p>
+              </div>
+              <div className="flex flex-row text-sm font-medium text-gray-400">
+                <p>We have sent a code to your email {email}</p>
+              </div>
             </div>
-            <div className="flex flex-row text-sm font-medium text-gray-400">
-              <p>We have sent a code to your email {email}</p>
-            </div>
-          </div>
 
-          <div>
-            <form action="#" method="post">
-              {/* <div className="flex flex-col space-y-16">
+            <div>
+              <form action="#" method="post">
+                {/* <div className="flex flex-col space-y-16">
                 <div className="flex flex-row items-center justify-between mx-auto w-full max-w-xs">
                   <div className="w-16 h-16">
                     <Input type="number" name="1" id="1" maxLength={1} />
@@ -120,42 +158,43 @@ export const OTP = ({ email }: Props) => {
                   </div>
                 </div>
               </div> */}
-              <OTPInput
-                shouldAutoFocus
-                inputType="number"
-                value={otp}
-                onChange={setOtp}
-                numInputs={6}
-                renderSeparator={
-                  <span className="px-[2px] text-gray-500">-</span>
-                }
-                renderInput={(props) => <Input {...props} />}
-                onPaste={handlePaste}
-              />
-              <div className="pt-8">
-                <Button
-                  color="primary"
-                  fullWidth
-                  onClick={verifyOTP}
-                  isLoading={isLoading}
-                >
-                  Confirm
-                </Button>
-              </div>
-              {isLoading && (
-                <div className="pt-4">
-                  <Progress
+                <OTPInput
+                  shouldAutoFocus
+                  inputType="number"
+                  value={otp}
+                  onChange={setOtp}
+                  numInputs={6}
+                  renderSeparator={
+                    <span className="px-[2px] text-gray-500">-</span>
+                  }
+                  renderInput={(props) => <Input {...props} />}
+                  onPaste={handlePaste}
+                />
+                <div className="pt-8">
+                  <Button
                     color="primary"
-                    size="sm"
-                    aria-label="Loading..."
-                    value={progress}
-                  />
+                    fullWidth
+                    onClick={verifyOTP}
+                    isLoading={isLoading}
+                  >
+                    Confirm
+                  </Button>
                 </div>
-              )}
-            </form>
+                {isLoading && (
+                  <div className="pt-4">
+                    <Progress
+                      color="primary"
+                      size="sm"
+                      aria-label="Loading..."
+                      value={progress}
+                    />
+                  </div>
+                )}
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
