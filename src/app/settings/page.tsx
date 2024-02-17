5bf6f05c54b6a321d1d19582f3838e1ed9c8ecbe
Kim Hwanhoon <kimhwanhoon@icloud.com>
2024-02-17 12:07:03 +0100

    settings editted

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,5 +1,12 @@
+import { createClient } from "@/utils/supabase/client"
 import react from "react"
 
 const SettingsPage = async() => {
-    return <div>Settings</div>
+    const supabase = createClient()
+    const signout = async()=> {
+        await supabase.auth.signOut()
+    }
+    return <div>Settings<button onClick={()=>signout()}>Sign out</button></div>
 }
+
+export default SettingsPage
