4fc011a701199cebb1e86ed77220c9aabb629d5d
Kim Hwanhoon <kimhwanhoon@outlook.com>
2024-02-19 18:38:09 +0100

    words search added

--- src/app/(words)/words/page.tsx
+++ src/app/(words)/words/page.tsx
@@ -5,6 +5,7 @@ const WordsPage = () => {
     <div className="flex flex-col p-4 bg-white h-dvh">
       <Link href={"/words/add"}>Add</Link>
       <Link href={"/words/list"}>List</Link>
+      <Link href={"/words/search"}>Search</Link>
     </div>
   );
 };
