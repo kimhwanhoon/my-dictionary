862e395b481713d5ccb23127b577245fb052cc31
Kim Hwanhoon <kimhwanhoon@outlook.com>
2024-03-31 22:00:07 +0200

    dictionary ongoing

--- src/app/dictionary/components/SearchResult.tsx
+++ src/app/dictionary/components/SearchResult.tsx
@@ -4,7 +4,6 @@ import { capitalizeFirstLetter } from "@/utils/dictionary/capitalizeFirstLetter"
 import { extractPronunciation } from "@/utils/dictionary/extractPronunciation";
 import { extractTextBetweenTags } from "@/utils/dictionary/extractTextBetweenTags";
 import { removeTitle } from "@/utils/dictionary/removeTitle";
-import { searchDictionary } from "@/utils/dictionary/search";
 import { Buttons } from "./Buttons";
 import parse from "html-react-parser";
 import { useEffect, useState } from "react";
@@ -24,8 +23,15 @@ export const SearchResult = ({ search, lang }: Props) => {
 
   useEffect(() => {
     const getData = async () => {
-      const data = await searchDictionary({ word: search, lang });
-      if (!data) {
+      const body = JSON.stringify({ word: search, lang });
+      const res = await fetch("/api/dictionary/search", {
+        body,
+        method: "post",
+      });
+      const { error, data } = await res.json();
+
+      // const data = await searchDictionary({ word: search, lang });
+      if (error) {
         setIsSuccess(false);
       } else {
         setIsSuccess(true);
@@ -37,29 +43,31 @@ export const SearchResult = ({ search, lang }: Props) => {
 
   useEffect(() => {
     if (theme === "light") {
-      setBackgroundColor("dictionary-result-background");
+      setBackgroundColor("light-dictionary-result-background");
     } else {
       setBackgroundColor("dark-dictionary-result-background");
     }
   }, [theme]);
 
-  const resultBefore: string = (isSuccess && data.entryContent) ?? "";
+  const resultBefore: string = (isSuccess && data) ?? "";
   const resultWord = extractTextBetweenTags(resultBefore) ?? "";
   const pronunciation = extractPronunciation(resultBefore) ?? "";
   const titleRemoved = removeTitle(resultBefore) ?? "";
 
   return (
     <div
-      className={`h-[calc(100dvh-212px)] flex flex-col p-4 pt-8 ${backgroundColor}`}
+      className={`min-h-[calc(100dvh-212px)] flex flex-col p-4 pt-8 ${backgroundColor}`}
     >
       <section className="flex items-center justify-between ">
         {isSuccess && (
           <>
             <div className="flex gap-2 items-center">
               <h1 className="text-4xl font-semibold pr-2">
-                capitalizeFirstLetter(resultWord)
+                {capitalizeFirstLetter(resultWord)}
               </h1>
-              <span className="text-xs text-gray-700">[{pronunciation}]</span>
+              <span className="text-xs text-gray-700 dark:text-gray-200">
+                [{pronunciation}]
+              </span>
             </div>
             <Buttons word={resultWord as string} />
           </>
